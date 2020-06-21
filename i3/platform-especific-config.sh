04343a0cf587c02c6a814bc1e9bf6233e67bd4dc
rayenok <sk0l3m@gmail.com>
2020-06-21 16:10:27 +0200

    added custom config for specific computers

---
+++ i3/platform-especific-config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# mount --bind /mnt/dropbox/org/agenda /mnt/agenda
+# mount --bind /home/rayenok/Dropbox/org /mnt/dropbox/org
+# mount --bind /mnt/dropbox/org/it/security /mnt/security
+variety &
